8e6124a323f253b46aa973b31fbf16b56cea4800
Hem Raj Regmi <sangamsyabil@gmail.com>
2025-01-10 17:34:32 -0500

    day one done

---
+++ Day 1/task.py
@@ -0,0 +1,28 @@
+# Single line printing
+print("Hello World!")
+
+# Multiple lines printing
+print("Hello World! \nHello World!")
+
+# Input lines
+print("Hello " + input("Enter your name: ") + "!")
+
+# Python variables
+name = "John Doe"
+age = 30
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+
+username = input("Enter your username: ")
+length = len(username)
+print(length)
+
+glass1 = "milk"
+glass2 = "water"
+
+temp_var = glass2
+glass2 = glass1
+glass1 = temp_var
+
